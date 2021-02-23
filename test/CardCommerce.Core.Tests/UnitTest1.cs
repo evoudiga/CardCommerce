50ecdc7d66ca5333d71038003a128a793640ebd6
E36721 <ellivoud@yahoo.gr>
2021-02-23 14:27:31 +0200

    Try test 1

--- test/CardCommerce.Core.Tests/UnitTest1.cs
+++ test/CardCommerce.Core.Tests/UnitTest1.cs
@@ -1,5 +1,8 @@
 using System;
 using Xunit;
+using System.Linq;
+using CardCommerce.Core.Data;
+using CardCommerce.Core.Model;
 
 namespace CardCommerce.Core.Tests
 {
@@ -8,7 +11,46 @@ namespace CardCommerce.Core.Tests
         [Fact]
         public void Test1()
         {
+            using var dbContext = new CardDBContext();
+
+            var card1 = new Card()
+            {
+                CardNumber = "1111111111111111",
+                AvailBalance = 1500M
+            };
+
+            card1.Trn1 = new Transaction()
+            {
+                TrnType = TransactionType.Ecommerce,
+                LimitAm0 = 200M,
+                LimitAm1 = 0M,
+                AggregateAmount = 200M,
+                CreatedDate = DateTimeOffset.Now.Date.ToString("yyyy-MM-dd")
+            };
+
+            card1.Trn.Add(new Transaction()
+            {
+                TrnType = TransactionType.CardPresent,
+                LimitAm0 = 200M,
+                LimitAm1 = 700M,
+                AggregateAmount = 900M,
+                CreatedDate = DateTimeOffset.Now.Date.ToString("yyyy-MM-dd")
+            });
+           
+
+            dbContext.Add(card1);
+            dbContext.SaveChanges();
+
+            
+
 
         }
     }
+
+    public static bool isGreater(decimal i0, decimal i1, decimal AvailBalance)
+    {
+        return i > j;
+    }
 }
+
+
